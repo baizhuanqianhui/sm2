85d15b534eb51f68eabe0c7126d418fb3dfbe6d5
yangxuewei <yang_xuehui@163.com>
2018-01-13 20:35:50 +0800

    SM2，SM3，SM4 初始提交

---
+++ src/main/java/sm4/SM4_Context.java
@@ -0,0 +1,19 @@
+package sm4;
+
+/**
+ * Created by zhuxiaole on 2018/1/13.
+ */
+public class SM4_Context {
+    public int mode;
+
+    public long[] sk;
+
+    public boolean isPadding;
+
+    public SM4_Context()
+    {
+        this.mode = 1;
+        this.isPadding = true;
+        this.sk = new long[32];
+    }
+}
