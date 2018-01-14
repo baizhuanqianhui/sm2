921678a0ed82af66f1dd2b8216fc9088b21c0185
yangxuewei <yang_xuehui@163.com>
2018-01-14 23:54:15 +0800

    SM2，SM3，SM4 可以生成cer/sm2/csr文件

--- src/main/java/base/SM2AlgorithmIdentifier.java
+++ src/main/java/base/SM2AlgorithmIdentifier.java
@@ -7,16 +7,29 @@ import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
  * Created by zhuxiaole on 2018/1/13.
  */
 public class SM2AlgorithmIdentifier extends AlgorithmIdentifier {
+    private ASN1ObjectIdentifier algorithm;
+    private ASN1Encodable       parameters;
 
     public SM2AlgorithmIdentifier(ASN1ObjectIdentifier algorithm) {
         super(algorithm);
+        this.algorithm = algorithm;
     }
 
     public SM2AlgorithmIdentifier(ASN1ObjectIdentifier algorithm,ASN1Encodable parameters)
     {
         super(algorithm,parameters);
+        this.algorithm = algorithm;
+        this.parameters = parameters;
+    }
+    public ASN1ObjectIdentifier getAlgorithm()
+    {
+        return algorithm;
     }
 
+    public ASN1Encodable getParameters()
+    {
+        return parameters;
+    }
 
     @Override
     public ASN1Primitive toASN1Primitive() {
@@ -25,9 +38,9 @@ public class SM2AlgorithmIdentifier extends AlgorithmIdentifier {
         ASN1EncodableVector v = new ASN1EncodableVector();
 
         v.add(algorithm);
-        if (getParameters() != null)
+        if (parameters != null)
         {
-            v.add(getParameters());
+            v.add(parameters);
         }
 
         return new DERSequence(v);
